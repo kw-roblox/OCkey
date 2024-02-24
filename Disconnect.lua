7b4acf5b7df3ab77ec7227df3d0363c4e054e38c
kw-roblox <136266319+kw-roblox@users.noreply.github.com>
2024-02-24 11:57:11 +0000

    Update Disconnect.lua

--- Disconnect.lua
+++ Disconnect.lua
@@ -1,19 +1,5 @@
-Players = {
-  nil
+Users = {
+  "jkml2000"
 }
-All = false
 
-if Players == nil then
-  All = true
-end
-
-if All == true then
-  game.Players.LocalPlayer:Kick("Access Denied")
-end
-for i, v in pairs(Players) do
-
-  if game.Players.LocalPlayer == v then
-    game.Players.LocalPlayer:Kick("Access Denied")
-  end
-
-end
+return Users
