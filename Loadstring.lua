6362fc9993eb356e36fc1ea313655311c877aa5a
kw-roblox <136266319+kw-roblox@users.noreply.github.com>
2024-03-29 13:10:11 +0000

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-"https://gist.githubusercontent.com/kw-roblox/ca85436b1326d24285d91e0666f08b46/raw/951dff953182f35f3585597075b24660585b4934/key.lua"
+"https://gist.githubusercontent.com/kw-roblox/ca85436b1326d24285d91e0666f08b46/raw/5de10eca52cca4fb97bc565bc1aa625fcf3ff9e7/key.lua"
